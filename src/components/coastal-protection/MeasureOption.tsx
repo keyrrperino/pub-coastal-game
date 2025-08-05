e346934e7bede9a61fc9e3e5625522d78014a09d
Ronnel Marfil <ronnel@symph.co>
2025-08-05 23:58:42 +0800

    [Adjustment][Ronnel] Update MeasureOption styles for consistency.
    Modified `MeasureOption` styles to include a specific radial gradient position, added border radius, and adjusted box shadow properties. These changes improve compatibility with design standards and ensure visual consistency across components.

--- src/components/coastal-protection/MeasureOption.tsx
+++ src/components/coastal-protection/MeasureOption.tsx
@@ -19,8 +19,9 @@ const MeasureOption: React.FC<MeasureOptionProps> = ({
         onClick={onClick}
         className="flex flex-col justify-center items-center gap-[12.66px] px-[25.31px] w-[76px] h-[76px] rounded-[63.28px]"
         style={{
-          background: 'radial-gradient(circle, #B6FFF3 0%, #14F4CF 100%)',
-          boxShadow: '0px 5.91px 29.56px 0px rgba(0, 0, 0, 0.15)'
+          background: 'radial-gradient(circle at 10.5% 16.4%, #B6FFF3 0%, #14F4CF 100%)',
+          boxShadow: '0px 5.91px 29.56px 0px rgba(0, 0, 0, 0.15)',
+          borderRadius: '63.28px'
         }}
       >
         <div className={`text-center text-[14px] font-bold leading-[14px] ${styles.novecentoBold} uppercase`} 
